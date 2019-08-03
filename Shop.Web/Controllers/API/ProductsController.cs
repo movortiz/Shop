aa19c6dd4055a4767f29dd1748cc9f6895548297
Marco Ortiz <vmarco98@hotmail.com>
2019-08-03 09:54:53 -0600

    Added The GetProducts API

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+
+        public IProductRepository productRepository { get; }
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts2()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
