f7ecd103052bef36e28e3c0ab6307f8eb2e7e043
Marco Ortiz <vmarco98@hotmail.com>
2019-08-04 21:44:05 -0600

    Started to work on the FrontEnd. Added the Class InstanceLocator,  MainViewModel , LoginViewModel and its bindings.

---
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.UIForms.Infrastructure
+{
+    using Shop.UIForms.ViewModels;
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+            this.Email = "marco.vinicio.ortiz@gmail.com";
+            this.Password = "123458";
+        }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Please enter a valid email address",
+                    "Ok"
+                    );
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Password cannot be empty",
+                    "Ok"
+                    );
+                return;
+            }
+
+            if(!this.Email.Equals("marco.vinicio.ortiz@gmail.com") || !this.Password.Equals("123458"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Invalid Email/Password Combination",
+                    "Ok"
+                    );
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Test passed",
+                    "Ok"
+                    );
+            return;
+        }
+    }
+}
