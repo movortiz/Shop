92c0044c901e31db9a6f17ae2c9ba16d62d68a6f
Marco Ortiz <vmarco98@hotmail.com>
2019-08-05 17:56:20 -0600

    API Service Creation in Common Class

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -63,17 +63,19 @@
 
                 if (view.ImageFile != null && view.ImageFile.Length > 0)
                 {
+                    var guid = Guid.NewGuid().ToString();
+                    var file = $"{guid}.jpg";
                     path = Path.Combine(
-                        Directory.GetCurrentDirectory(), 
-                        "wwwroot\\images\\Products", 
-                        view.ImageFile.FileName);
+                                        Directory.GetCurrentDirectory(),
+                                                "wwwroot\\images\\Products",
+                                        file);
 
                     using (var stream = new FileStream(path, FileMode.Create))
                     {
                         await view.ImageFile.CopyToAsync(stream);
                     }
 
-                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                    path = $"~/images/Products/{file}";
                 }
 
                 var product = this.ToProduct(view, path);
@@ -134,14 +136,19 @@
 
                     if (view.ImageFile != null && view.ImageFile.Length > 0)
                     {
-                        path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\Products", view.ImageFile.FileName);
+                        var guid = Guid.NewGuid().ToString();
+                        var file = $"{guid}.jpg";
+                        path = Path.Combine(
+                                            Directory.GetCurrentDirectory(),
+                                                    "wwwroot\\images\\Products",
+                                            file);
 
                         using (var stream = new FileStream(path, FileMode.Create))
                         {
                             await view.ImageFile.CopyToAsync(stream);
                         }
 
-                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                        path = $"~/images/Products/{file}";
                     }
 
                     // TODO: Pending to change to: this.User.Identity.Name
